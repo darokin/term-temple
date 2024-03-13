04a4f3f446e566d47d36d87fd99e96f098aa9278
darokin <darokin42@gmail.com>
2024-03-13 22:41:27 +0100

    widgetANSI cleanup

--- src/widgetANSI.cpp
+++ src/widgetANSI.cpp
@@ -3,29 +3,18 @@
 #include "widgetANSI.hpp"
 #include "cursesAnsi.hpp"
 
-/*
-// TODO : Mettre un moduleANSI inside et avoir un draw d'une ligne
-WidgetANSI::WidgetANSI(const std::wstring& _title, i2d _pos, i2d _size) : Widget::Widget(_title, _pos, _size) {
-}
-
-WidgetANSI::WidgetANSI(const std::wstring& _title) : Widget::Widget(_title) {
-}
-*/
-
 WidgetANSI::WidgetANSI(const std::wstring& _title, const char* _ansiFilePath) : Widget::Widget(_title) {
-    // , ansiModule { _ansiFilePath }
+    // == Init ANSI module and set position
     ansiModule = std::make_unique<ModuleANSI>(_ansiFilePath);
     i2d ansiSize = ansiModule->getSize();
     ansiSize.x += 2;
     ansiSize.y += 2;
+    // == Set widget size according to module ansi content (size), so before updatePos()
     this->setSize(ansiSize);
     ansiModule->setWidget(this);
     ansiModule->updatePos();
-    //this->bBorder = false;
+    // == Widget Color
     this->setColorPair(getColor(colorPairs::WHITE_ON_BLACK));
-    //ansiFile.open(_ansiFilePath);
-    //if (!ansiFile.is_open())
-    //    std::cout << "Unable to open ANSI file '" << _ansiFilePath << "'" << std::endl; 
 }
 
 WidgetANSI::~WidgetANSI() {
@@ -34,19 +23,6 @@ WidgetANSI::~WidgetANSI() {
 
 void WidgetANSI::draw() {
     ansiModule->draw();
-    /*
-    if (!ansiFile.is_open())
-        return;
-    std::string _line;
-    int y { 0 };
-    this->ansiFile.clear();
-    this->ansiFile.seekg(0);
-    while (getline(this->ansiFile, _line) && y < this->size.y) {
-        if (!cursesANSI::mvwaddnANSIstr(this->pos.y + y, this->pos.x + 1, _line))
-            break;
-        y++;
-    }
-    */
 }
 
 void WidgetANSI::setPos(i2d _pos) {
